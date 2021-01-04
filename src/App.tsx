d56ad0606a323ee9914f72683a8ab6b11378fc10
Aleksandr Sharikov <notsaint.mail@gmail.com>
2021-01-04 20:08:01 +0300

    chore: start app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+const App = (): React.ReactElement => {
+  return (
+    <div>
+      <header>
+        <h1>Simple Daily Notes</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
